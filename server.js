2ebb566471bf82b1e993734309af2fdeaff4434c
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-18 13:49:19 +0200

    Fetch nature

--- server.js
+++ server.js
@@ -75,4 +75,28 @@ app.get('/mountains', (req, res) => {
     getFlickrPhotos()
 })
 
+app.get('/nature', (req, res) => {
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        '*'
+    )
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    const getFlickrPhotos = async () => {
+    const flickr = new Flickr('57024ec69177dc3efff9fb4f22635635')
+    flickr.photos.search({
+        tags: 'nature'
+    }).then(function (response) {
+        console.log('Success!', response.body)
+        const images = response.body
+        res.send(images)
+    }).catch(function (err) {
+        console.error('Error!', err)
+    })
+    }
+    getFlickrPhotos()
+})
+
 app.listen(3000)
