65070e91bdc15ca2156791d8a789fc01c19ea212
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-18 12:08:37 +0200

    Added fetch forests

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const Flickr = require('flickr-sdk')
 
-app.get('/images', (req, res) => {
+app.get('/rivers', (req, res) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
         '*'
@@ -16,6 +16,30 @@ app.get('/images', (req, res) => {
     const flickr = new Flickr('57024ec69177dc3efff9fb4f22635635')
     flickr.photos.search({
         tags: 'river'
+    }).then(function (response) {
+        console.log('Success!', response.body)
+        const images = response.body
+        res.send(images)
+    }).catch(function (err) {
+        console.error('Error!', err)
+    })
+    }
+    getFlickrPhotos()
+})
+
+app.get('/forests', (req, res) => {
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        '*'
+    )
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    const getFlickrPhotos = async () => {
+    const flickr = new Flickr('57024ec69177dc3efff9fb4f22635635')
+    flickr.photos.search({
+        tags: 'forest'
     }).then(function (response) {
         console.log('Success!', response.body)
         const images = response.body
