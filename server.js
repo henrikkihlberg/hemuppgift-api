05bb326c624287a078669d4339f23eb2c2965935
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-18 09:37:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const Flickr = require('flickr-sdk')
 
-app.get('/', (req, res) => {
+app.get('/images', (req, res) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
         '*'
