a04e6b9aea06e0944fd1b458842a78da9b567a91
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-10-20 09:02:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,100 +4,113 @@ require('dotenv').config()
 
 const Flickr = require('flickr-sdk')
 
-app.get('/rivers', (req, res) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    )
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    const getFlickrPhotos = async () => {
-    const flickr = new Flickr(process.env.FLICKR_API_KEY)
-    flickr.photos.search({
-        tags: 'river'
-    }).then(function (response) {
-        console.log('Success!', response.body)
-        const images = response.body
-        res.send(images)
-    }).catch(function (err) {
-        console.error('Error!', err)
+const fetchImages = () => {
+    var flickrData = [
+        {param: '/rivers', tags: 'river'},
+        {param: '/forests', tags: 'forest'},
+        {param: '/mountains', tags: 'mountain'},
+        {param: '/nature', tags: 'nature'}
+    ]
+    flickrData.forEach((info) => {
+        app.get(info.param, (req, res) => {
+            res.setHeader(
+                'Access-Control-Allow-Origin',
+                '*'
+            )
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            )
+            const getFlickrPhotos = async () => {
+            const flickr = new Flickr(process.env.FLICKR_API_KEY)
+            flickr.photos.search({
+                tags: info.tags
+            }).then(function (response) {
+                console.log('Success!', response.body)
+                const images = response.body
+                res.send(images)
+            }).catch(function (err) {
+                console.error('Error!', err)
+            })
+            }
+            getFlickrPhotos()
+        })
     })
-    }
-    getFlickrPhotos()
-})
+}
+fetchImages()
 
-app.get('/forests', (req, res) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    )
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    const getFlickrPhotos = async () => {
-    const flickr = new Flickr(process.env.FLICKR_API_KEY)
-    flickr.photos.search({
-        tags: 'forest'
-    }).then(function (response) {
-        console.log('Success!', response.body)
-        const images = response.body
-        res.send(images)
-    }).catch(function (err) {
-        console.error('Error!', err)
-    })
-    }
-    getFlickrPhotos()
-})
 
-app.get('/mountains', (req, res) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    )
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    const getFlickrPhotos = async () => {
-    const flickr = new Flickr(process.env.FLICKR_API_KEY)
-    flickr.photos.search({
-        tags: 'mountain'
-    }).then(function (response) {
-        console.log('Success!', response.body)
-        const images = response.body
-        res.send(images)
-    }).catch(function (err) {
-        console.error('Error!', err)
-    })
-    }
-    getFlickrPhotos()
-})
 
-app.get('/nature', (req, res) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    )
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    const getFlickrPhotos = async () => {
-    const flickr = new Flickr(process.env.FLICKR_API_KEY)
-    flickr.photos.search({
-        tags: 'nature'
-    }).then(function (response) {
-        console.log('Success!', response.body)
-        const images = response.body
-        res.send(images)
-    }).catch(function (err) {
-        console.error('Error!', err)
-    })
-    }
-    getFlickrPhotos()
-})
+// app.get('/forests', (req, res) => {
+//     res.setHeader(
+//         'Access-Control-Allow-Origin',
+//         '*'
+//     )
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     const getFlickrPhotos = async () => {
+//     const flickr = new Flickr(process.env.FLICKR_API_KEY)
+//     flickr.photos.search({
+//         tags: 'forest'
+//     }).then(function (response) {
+//         console.log('Success!', response.body)
+//         const images = response.body
+//         res.send(images)
+//     }).catch(function (err) {
+//         console.error('Error!', err)
+//     })
+//     }
+//     getFlickrPhotos()
+// })
+
+// app.get('/mountains', (req, res) => {
+//     res.setHeader(
+//         'Access-Control-Allow-Origin',
+//         '*'
+//     )
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     const getFlickrPhotos = async () => {
+//     const flickr = new Flickr(process.env.FLICKR_API_KEY)
+//     flickr.photos.search({
+//         tags: 'mountain'
+//     }).then(function (response) {
+//         console.log('Success!', response.body)
+//         const images = response.body
+//         res.send(images)
+//     }).catch(function (err) {
+//         console.error('Error!', err)
+//     })
+//     }
+//     getFlickrPhotos()
+// })
+
+// app.get('/nature', (req, res) => {
+//     res.setHeader(
+//         'Access-Control-Allow-Origin',
+//         '*'
+//     )
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     const getFlickrPhotos = async () => {
+//     const flickr = new Flickr(process.env.FLICKR_API_KEY)
+//     flickr.photos.search({
+//         tags: 'nature'
+//     }).then(function (response) {
+//         console.log('Success!', response.body)
+//         const images = response.body
+//         res.send(images)
+//     }).catch(function (err) {
+//         console.error('Error!', err)
+//     })
+//     }
+//     getFlickrPhotos()
+// })
 
 app.listen(8080)
